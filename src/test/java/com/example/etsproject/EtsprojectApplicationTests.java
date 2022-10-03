f27c4ab4e2dff25f27ead2d423cadf0022542728
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-10-03 16:02:19 +0300

    init project.

---
+++ src/test/java/com/example/etsproject/EtsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtsprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
