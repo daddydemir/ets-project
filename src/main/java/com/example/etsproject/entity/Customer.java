e9f7cb67800645d3bb69137b9faec11329a6de9d
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-10-11 20:50:25 +0300

    customer email data has been hidden.

--- src/main/java/com/example/etsproject/entity/Customer.java
+++ src/main/java/com/example/etsproject/entity/Customer.java
@@ -1,7 +1,6 @@
 package com.example.etsproject.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +28,7 @@ public class Customer {
     @Column(name = "password")
     private String password;
 
+    @JsonIgnore
     @Column(name = "email", unique = true)
     private String email;
 
